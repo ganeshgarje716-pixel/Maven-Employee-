6e93c207631acc68e1e90745c43ffb91ce943a4e
Ganesh <ganeshgarje716@gmail.com>
2026-04-26 14:23:48 +0530

    Create JDBC Connection

--- EmployeeJDBC/src/main/java/com/jdbcConnection/JDBCUtil.java
+++ EmployeeJDBC/src/main/java/com/jdbcConnection/JDBCUtil.java
@@ -1,5 +1,36 @@
 package com.jdbcConnection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCUtil {
+	
+	
+	
+	private static final String url="jdbc:mysql://localhost:3306/jdbc";
+	
+	private static final String username="root";
+	
+	private static final String password="garje@716Garje";
+	
+	
+	
+	public static Connection createConnection() {
+		
+		Connection con=null;
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.DSriver");
+			
+			 con = DriverManager.getConnection(url, username, password);
+		} 
+		catch (ClassNotFoundException | SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
 
 }
